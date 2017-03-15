5bc69b93ad28d43c211135bb07e75d40560f4b26
Minato4th <borisko.aleksey@gmail.com>
2017-03-15 23:01:19 +0200

    Update data

--- src/git.java
+++ src/git.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Jin on 15.03.2017.
  */
 public class git {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
 }
